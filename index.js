ab0655c73b55031e5f9627f5f20b5f3e9841b51b
James Njenga <knjenga94@gmail.com>
2022-08-20 22:28:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(d){
+
+    if(d>42){
+        return d-42;
+     }
+
+    else{
+        return 42-d;
+    }
+
+}
+
+const dB=distanceFromHqInBlocks(43);
+
+console.log("The Db dist is"+dB);
+
+
+function distanceFromHqInFeet(dFeet){
+
+    //const feetToDb=264*(dFeet-42);
+    const feetToDb=264*(distanceFromHqInBlocks(dFeet));
+
+     return feetToDb;
+
+}
+
+function distanceTravelledInFeet(start,destination){
+    
+    if(destination>start){
+        return 264*(destination-start);
+    }else{
+        return 264*(start-destination);
+    }
+
+}
+
+function calculatesFarePrice(start, destination){
+
+const totalDistance=distanceTravelledInFeet(start,destination);
+
+let chargePerFoot;
+
+if(totalDistance<=400){
+    return chargePerFoot=0;
+}
+else if(totalDistance>400 && totalDistance<=2000){
+   return (totalDistance-400)*0.02
+
+}else if(totalDistance>2000 && totalDistance<=2500){
+    chargePerFoot=25;
+    return chargePerFoot;
+
+}else if(totalDistance>2500){
+    chargePerFoot="cannot travel that far";
+    return chargePerFoot;
+}
+
+}
+
+
+
